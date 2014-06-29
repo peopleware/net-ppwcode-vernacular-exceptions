cafff68cfc33d650e3e04228039dbe6db9d5e7e9
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2014-06-29 23:33:08 +0200

    Improved metadata.

--- src/II/Properties/AssemblyInfo.cs
+++ src/II/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 #endregion
 
 [assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.II")]
-[assembly: AssemblyDescription("Main code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyDescription("PPWCode Vernacular Exceptions")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a96a19e0-b3e8-4181-a2be-9518870d63d8")]

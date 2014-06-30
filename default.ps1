fceb114ce31156a503c2eb1fa96d1993901171ec
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2014-06-30 03:03:40 +0200

    Initial version of psake configuration.

---
+++ default.ps1
@@ -0,0 +1,118 @@
+﻿# Copyright 2014 by PeopleWare n.v..
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# psake build file
+#
+
+Framework "4.0"
+
+Properties {
+    $configuration = "Debug"
+    $repos = @("local", "nuget")
+}
+
+Task Default -depends ?
+
+Task ? {
+    WriteDocumentation
+}
+
+Task PackageRestore -depends PackageClean {
+    Push-Location
+
+    try
+    {
+        Write-Host "Restoring package dependencies." -ForegroundColor Green
+
+        Set-Location 'src'
+        $solution = Get-Item '*.sln' | Select-Object -First
+        $reposources = ""
+        $repos | ForEach-Object { $reposources="$reposources -source $_" } 
+        Exec { nuget restore "$solution.sln" $reposources }
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+Task PackageClean {
+    Push-Location
+
+    try
+    {
+        Write-Host "Clean package dependencies." -ForegroundColor Green
+
+        # clean up packages folder
+        Remove-Item -Path 'src\packages\*' -Exclude 'repositories.config' -Recurse -Force -ErrorAction Ignore
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+Task FullBuild -depends PackageRestore,Build
+
+Task Build -depends Clean {
+    Push-Location
+
+    try
+    {
+        Set-Location 'src'
+        Write-Host "Compiling." -ForegroundColor Green
+        $solution = Get-Item '*.sln' | Select-Object -First
+        Exec { msbuild "$($solution.Name)" /t:Build /m /p:Configuration=$configuration /v:quiet }
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+Task Clean {
+    Push-Location
+
+    try
+    {
+        Write-Host "Cleaning." -ForegroundColor Green
+
+        # msbuild clean
+        Write-Host "Cleaning solution" -ForegroundColor Green
+        Set-Location 'src'
+        $solution = Get-Item '*.sln' | Select-Object -First
+        Exec { msbuild "$($solution.Name)" /t:Clean /m /p:Configuration=$configuration /v:quiet }
+        Set-Location '..'
+
+        # clean up scratch folder
+        Write-Host "Cleaning scratch" -ForegroundColor Green
+        Remove-Item -Path 'scratch' -Recurse -Force -ErrorAction Ignore
+
+        # clean up bin/obj folders
+        Write-Host "Cleaning bin/obj" -ForegroundColor Green
+        Set-Location 'src'
+        Remove-Item -Path '*\bin','*\obj' -Recurse -Force -ErrorAction Ignore
+
+        # clean up packages folder
+        Write-Host "Cleaning packages" -ForegroundColor Green
+        Get-ChildItem -Directory | Where-Object { $_.Name -ne 'packages'  } |  Get-ChildItem -Filter '*.nupkg' -recurse | Remove-Item -Force -ErrorAction Ignore
+    }
+    finally
+    {
+        Pop-Location
+    }
+}
+
+Task RealClean -depends Clean,PackageClean

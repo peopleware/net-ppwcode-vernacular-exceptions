5d9934e90412bdd217ab3f8183d50c04a2a27cc2
Ruben Vandeginste <ruben.vandeginste@gmail.com>
2014-07-06 14:20:16 +0200

    Change copyright year to current year.

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware n.v.")]
 [assembly: AssemblyProduct("PPWCode Vernacular Exceptions")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare n.v. 2010-2014")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare n.v. 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0")]

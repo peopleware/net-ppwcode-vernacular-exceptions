74ca14b293abe40ecc9cc988a60ce09b778f7ea4
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-12 00:07:00 +0200

    Updated README.md file.

--- src/I/Properties/AssemblyInfo.cs
+++ src/I/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("PPWCode.Vernacular.Exceptions.I")]
-[assembly: AssemblyDescription("Main code of the PPWCode Exceptions Vernacular")]
+[assembly: AssemblyDescription("PPWCode.Vernacular.Exceptions.I")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a96a19e0-b3e8-4181-a2be-9518870d63d8")]

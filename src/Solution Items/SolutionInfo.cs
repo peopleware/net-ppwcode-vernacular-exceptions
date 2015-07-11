f11c60e8bfb5ecc1df1a5e3967e5891bcfa4fa8e
Ruben Vandeginste <ruben_vandeginste@peopleware.be>
2015-07-12 00:11:13 +0200

    Bumping version for release 2.2.0.

--- src/Solution Items/SolutionInfo.cs
+++ src/Solution Items/SolutionInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Peopleware NV")]
 [assembly: AssemblyProduct("PPWCode Vernacular Exceptions")]
-[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2011")]
+[assembly: AssemblyCopyright("Copyright © PeopleWare NV 2010-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0.0")]
